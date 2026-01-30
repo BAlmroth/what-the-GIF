7eaf6b7e8dbf1515eaa1cb2716e92216b99fdec9
Benita Almroth <benita.almroth@hotmail.com>
2026-01-30 13:33:49 +0100

    add: npm packages, working express server

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("alles good!");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
