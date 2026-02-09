77576002518bdad9ce44ead0f052d2055a298a81
Benita Almroth <benita.almroth@hotmail.com>
2026-02-09 18:29:20 +0100

    reintroduced custom styling of subtitles

--- services/subtitleApplier.js
+++ services/subtitleApplier.js
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 import fs from 'fs';
-import path, { format } from 'path';
+import path from 'path';
 import util from 'util';
 import { fileURLToPath } from 'url';
 
@@ -25,7 +25,11 @@ export const applySubtitlesToVideo = async (videoPath, subtitlePath, options= {}
     const outPath = path.join(tempDirectory, `video-subtitled-${timestamp}.mp4`);
 
 
-    const command = `ffmpeg -i "${videoPath}" -vf subtitles="${subtitlePath}" -c:a copy "${outPath}"`;
+    const escapedSubPath = subtitlePath.replace(/\\/g, '/').replace(/:/g, '\\:');
+
+    const forceStyle = `FontSize=${fontSize},PrimaryColour=&H${colorToHex(fontColor)}&,OutlineColour=&H${colorToHex(outlineColor)}&,Outline=${outlineWidth},Alignment=${position === 'top' ? 6 : 2}`;
+
+    const command = `ffmpeg -i "${videoPath}" -vf "subtitles='${escapedSubPath}':force_style='${forceStyle}'" -c:a copy "${outPath}"`;
 
     try {
         console.log('Applying subtitles to video with command:', command);
